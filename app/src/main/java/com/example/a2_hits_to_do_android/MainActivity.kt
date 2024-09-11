1aecde01a13454b0dc0160165014b19319cd4d31
stpbruzz <stpbruzz@gmail.com>
2024-09-11 16:17:46 +0700

    layout

---
+++ app/src/main/java/com/example/a2_hits_to_do_android/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.a2_hits_to_do_android
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.EditText
+
+class Task(description: String, flag: Boolean = false) {
+
+}
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        findViewById<Button>(R.id.add_button).setOnClickListener {
+            val taskDescription = findViewById<EditText>(R.id.task_description_input).text
+
+        }
+    }
+}
